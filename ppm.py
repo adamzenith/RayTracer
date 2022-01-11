d2e3e934883883ef68f5f0aee0f0085803c8989b
Matthias <matthias@pop-os.localdomain>
2022-01-11 15:22:06 +0100

    baby steps

--- ppm.py
+++ ppm.py
@@ -1,14 +1,19 @@
+import sys
+
 def ImageBase():
 
     #Image
     imageHeight = 256
     imageWidth = 256
 
-    print("P3\n" + str(imageWidth) + ' ' + str(imageHeight))
+    print("P3\n" + str(imageWidth) + ' ' + str(imageHeight) +"\n255")
     
     #Render
 
     for j in range(imageHeight-1,-1,-1):
+        #print("\rScanlines Remaining: " + str(j)+ " ",file=sys.stderr)
+        sys.stderr.write("\rScanlines Remaining: " + str(j)+ " ")
+        sys.stderr.flush()
         for i in range(imageWidth):
             r = i/(imageWidth-1)
             g = j / (imageHeight-1)
@@ -19,5 +24,6 @@ def ImageBase():
             ib = int(255.999*b)
 
             print(str(ir) + ' ' + str(ig) + ' ' + str(ib))
+    print("\nDone",file=sys.stderr)
     return None
 ImageBase()
